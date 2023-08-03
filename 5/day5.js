37acccccc18fe841ece35a3e85367d4fb9acc047
fortink01@gmail.com <kevin@MacBooks-MacBook-Air.local>
2023-08-02 20:11:01 -0400

    modified day 5, while loop

--- 5/day5.js
+++ 5/day5.js
@@ -1,12 +1,15 @@
-let number = Math.floor(Math.random() * 100);
+let number = Math.floor(Math.random() * 10);
 
-let input = prompt("Please enter your number");
+ let input = 0;
 
+while (number != input) {
+  let input = prompt("Please enter your number");
 
-if (input == number) {
-  alert("Congratulations!");
-} else if (input > number) {
-  alert("You guess was too high! Try again.");
-} else if (input < number) {
-  alert("Your guess was too low! Try again.");
+  if (input == number) {
+    alert("Congratulations!");
+  } else if (input > number) {
+    alert("You guess was too high! Try again.");
+  } else if (input < number) {
+    alert("Your guess was too low! Try again.");
+  }
 }
