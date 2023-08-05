da4d242de7055829c99a56106b74d05f563a2981
fortink01@gmail.com <kevin@MacBooks-MacBook-Air.local>
2023-08-04 22:10:58 -0400

    changes day 7, commented

--- 7/evenNumbers.js
+++ 7/evenNumbers.js
@@ -5,6 +5,8 @@ function getEvenNumbers() {
     let arr = []
     const number = process.argv[2]
     for (let i = 1; i <= number; i++) {
+        // modulo (%) divides a given number and whatever remains of the division will be the outcome
+        // due to the === only whatever divides by two with no remainder will be pushed into the array
         if(i % 2 === 0){
             arr.push(i)
         }
